da241db912c52674d4602ec12c227338a9d070e4
tg-king-in <1243221613@qq.com>
2023-01-16 17:50:57 +0800

    second commit

--- src/main/java/b.java
+++ src/main/java/b.java
@@ -3,14 +3,6 @@ import java.util.List;
 
 public class b {
     public static void main(String[] args) {
-        List<String> list = new ArrayList<>();
-        list.add("1111");
-        list.add("2222");
-        for (String s : list) {
-            System.out.println(s);
-        }
-        for (int i = 0; i < list.size(); i++) {
-            System.out.println(list.get(i));
-        }
+        System.out.println("你好!");
     }
 }
